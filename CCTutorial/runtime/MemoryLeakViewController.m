833e97f746f5e9889b6a9025124e1bf66d1d5a1a
“chenchao09261” <3>
2020-03-18 11:05:10 +0800

    添加内存泄露检测的相关说明

--- CCTutorial/runtime/MemoryLeakViewController.m
+++ CCTutorial/runtime/MemoryLeakViewController.m
@@ -22,7 +22,9 @@ static UIViewController *vc = nil;
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 30)];
     label.numberOfLines = 0;
     [self.view addSubview:label];
-    label.text = @"点击返回按钮，2秒过后，控制台会打印测试结果";
+    
+    label.text = @"点击返回按钮，2秒过后，控制台会打印测试结果。\n注意如果泄露对象是你不再使用的对象，那才算真正的内存泄露";
+    [label sizeToFit];
 }
 
 @end
